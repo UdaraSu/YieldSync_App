dfd4b9904b594412d3a93d742c155ca8fd72649a
Kaveesha Lanka <it22889942@my.sliit.lk>
2025-03-21 11:26:37 +0530

    initial commit

--- client/App.js
+++ client/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey WTF are u doing</Text>
+      <Text>Hey</Text>
       <StatusBar style="auto" />
     </View>
   );
