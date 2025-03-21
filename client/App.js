33f6700e712bc102da003b7df20cae5d76636919
Udara Ireshan <155006912+UdaraSu@users.noreply.github.com>
2025-03-21 18:58:31 +0530

    homepage update

--- client/App.js
+++ client/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import header from "./components/header"; 
+import footer from "./components/footer"; 
+import HomeScreen from "./pages/HomeScreen";
+import DetailsScreen from "./pages/DetailsScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="auto" backgroundColor="green" />
+      <Stack.Navigator
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="header" component={header} />
+        <Stack.Screen name="footer" component={footer} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
