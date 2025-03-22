0b810ce68ad83f071a51d5f15a672af739214bed
hansipaboda <it22141392@my.sliit.lk>
2025-03-22 14:55:00 +0530

    update

--- client/App.js
+++ client/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Dashboard from "./pages/dashboard";
+import DistributorForm from "./pages/add_distributor";
+import UpdateDistributor from "./pages/update_distributor"; // Correct import for update page
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Dashboard">
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="Add Distributor" component={DistributorForm} />
+        <Stack.Screen name="Update Distributor" component={UpdateDistributor} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
